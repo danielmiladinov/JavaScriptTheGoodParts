e505b58d73a7437bc98505ec1b38fa9010ca5f96
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-20 22:57:27 -0400

    Singletons can have their own private functions.

--- singleton.js
+++ singleton.js
@@ -1,5 +1,9 @@
 var singleton = function () {
     var privateVariable;
 
+    function privateFunction(x) {
+        /* perhaps do something with privateVariable... */
+    };
+
     return {};
 }();

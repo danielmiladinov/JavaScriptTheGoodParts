83fcb946e22df697e24d012e866c8f38fc448b75
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-20 23:12:12 -0400

    Power Constructors in JS, Step 3.

--- power_constructors.js
+++ power_constructors.js
@@ -9,4 +9,7 @@ Power Constructors
 
 2. Define some variables and functions.
  * These become private members of the object under construction
+
+3. Agument the object with privileged methods.
+ * These methods have unique access to the properties defined in Step 2.
 */

178532552ca403cd85cba3eccda5714420c4d55c
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-08-20 22:50:35 -0400

    Added some commentary.

--- digit_name.js
+++ digit_name.js
@@ -1,3 +1,6 @@
+// The outer function executes immediately, and it returns the inner function.
+// The inner function is a closure, and retains access to the names array for
+// as long as digit_names has scope.
 var digit_name = function () {
     var names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
